dc130d32185f42ee82b90757ee324896e9be76eb
knagis_cp <SND\knagis_cp>
2012-03-07 22:05:37 +0000

    Continued implementing parsing for groundspeak:cache extensions.

---
+++ Development/GeoTransformer.Extensibility/Gpx/GeocacheDescription.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * This file is part of GeoTransformer project (http://geotransformer.codeplex.com/).
+ * It is licensed under Microsoft Reciprocal License (Ms-RL).
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using XmlConvert = System.Xml.XmlConvert;
+
+namespace GeoTransformer.Gpx
+{
+    public class GeocacheDescription : GpxElementBase
+    {
+        private static Dictionary<XName, Action<GeocacheDescription, XAttribute>> _attributeInitializers = new Dictionary<XName, Action<GeocacheDescription, XAttribute>>
+        {
+            { "html", (o, a) => o.IsHtml = XmlConvert.ToBoolean(a.Value) },
+        };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeocacheDescription"/> class.
+        /// </summary>
+        public GeocacheDescription()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeocacheDescription"/> class.
+        /// </summary>
+        /// <param name="description">The description XML element.</param>
+        public GeocacheDescription(XElement description)
+        {
+            if (description == null)
+                return;
+
+            this.Initialize(description, _attributeInitializers, null);
+            this.Text = description.Value;
+        }
+
+        /// <summary>
+        /// Serializes this object in XML format.
+        /// </summary>
+        /// <param name="options">The serialization options.</param>
+        /// <param name="localName">Local name of the XML element to be created.</param>
+        /// <returns></returns>
+        public XElement Serialize(GpxSerializationOptions options, string localName)
+        {
+            if (string.IsNullOrEmpty(localName))
+                throw new ArgumentNullException("localName");
+
+            if (string.IsNullOrWhiteSpace(this.Text))
+                return null;
+
+            var el = new XElement(options.GeocacheNamespace + localName);
+            el.Value = this.Text;
+
+            el.Add(new XAttribute("html", this.IsHtml));
+
+            return el;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the text in this instance is HTML.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is HTML; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsHtml
+        {
+            get { return this.GetValue<bool>("IsHtml"); }
+            set { this.SetValue("IsHtml", value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the text of the description.
+        /// </summary>
+        public string Text
+        {
+            get { return this.GetValue<string>("Text"); }
+            set { this.SetValue("Text", value); }
+        }
+    }
+}
---
+++ Development/GeoTransformer.Extensibility/Gpx/GeocacheImage.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * This file is part of GeoTransformer project (http://geotransformer.codeplex.com/).
+ * It is licensed under Microsoft Reciprocal License (Ms-RL).
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using XmlConvert = System.Xml.XmlConvert;
+
+namespace GeoTransformer.Gpx
+{
+    /// <summary>
+    /// Contains information about a image attached to a geocache.
+    /// </summary>
+    public class GeocacheImage : GpxElementBase
+    {
+        private static Dictionary<XName, Action<GeocacheImage, XElement>> _elementInitializers = new Dictionary<XName, Action<GeocacheImage, XElement>>
+        {
+            { XmlExtensions.GeocacheSchema_1_0_2 + "name", (o, e) => o.Title = e.Value },
+            { XmlExtensions.GeocacheSchema_1_0_2 + "url", (o, e) => o.Address = new Uri(e.Value) },
+        };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeocacheImage"/> class.
+        /// </summary>
+        public GeocacheImage()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeocacheImage"/> class.
+        /// </summary>
+        /// <param name="type">The geocache image XML element.</param>
+        public GeocacheImage(XElement image)
+        {
+            if (image == null)
+                return;
+
+            this.Initialize(image, null, _elementInitializers);
+        }
+
+        /// <summary>
+        /// Serializes the geocache image to XML format.
+        /// </summary>
+        /// <param name="options">The serialization options.</param>
+        /// <returns>The serialized data or <c>null</c> if this instance is empty.</returns>
+        public XElement Serialize(GpxSerializationOptions options)
+        {
+            if (options.GeocacheVersion != GeocacheVersion.Geocache_1_0_2 && !options.EnableUnsupportedExtensions)
+                return null;
+
+            var el = new XElement(XmlExtensions.GeocacheSchema_1_0_2 + "image");
+            if (!string.IsNullOrWhiteSpace(this.Title))
+                el.Add(new XElement(XmlExtensions.GeocacheSchema_1_0_2 + "name", this.Title));
+            if (this.Address != null)
+                el.Add(new XElement(XmlExtensions.GeocacheSchema_1_0_2 + "url", this.Address));
+
+            if (el.IsEmpty)
+                return null;
+
+            return el;
+        }
+
+        /// <summary>
+        /// Parses the contents of <c>groundspeak:images</c> element and returns a collection of images (one for each child element).
+        /// </summary>
+        /// <param name="attributes">The <c>groundspeak:images</c> XML element.</param>
+        /// <returns>A collection with one <see cref="GeocacheImage"/> instance per XML element.</returns>
+        public static ObservableCollection<GeocacheImage> Parse(XElement images)
+        {
+            var col = new ObservableCollection<GeocacheImage>();
+            if (images == null)
+                return col;
+
+            foreach (var elem in images.Elements())
+            {
+                if (string.Equals(elem.Name.LocalName, "image", StringComparison.Ordinal))
+                    col.Add(new GeocacheImage(elem));
+            }
+
+            return col;
+        }
+
+        /// <summary>
+        /// Gets or sets the title of the image.
+        /// </summary>
+        public string Title
+        {
+            get { return this.GetValue<string>("Title"); }
+            set { this.SetValue("Title", value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the URL address of the image.
+        /// </summary>
+        public Uri Address
+        {
+            get { return this.GetValue<Uri>("Address"); }
+            set { this.SetValue("Address", value); }
+        }
+    }
+}
