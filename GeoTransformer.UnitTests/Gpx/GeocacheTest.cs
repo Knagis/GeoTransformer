478431886dc15ef730a5b1349b52a21916e7d6a0
Kārlis Gaņģis <Knagis@users.noreply.github.com>
2012-03-24 18:09:22 +0000

    Geocache.IsDefined() now caches its return value. The cache is invalidated as soon as any property within it is changed (due to the nature of ObservableElement this also includes any property of nested objects).

---
+++ GeoTransformer.UnitTests/Gpx/GeocacheTest.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * This file is part of GeoTransformer project (http://geotransformer.codeplex.com/).
+ * It is licensed under Microsoft Reciprocal License (Ms-RL).
+ */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeoTransformer.Gpx;
+using System.Xml.Linq;
+
+namespace GeoTransformer.UnitTests.Gpx
+{
+    [TestClass]
+    public class GeocacheTest
+    {
+        /// <summary>
+        /// Tests the <see cref="Geocache.IsDefined"/> method.
+        /// </summary>
+        [TestMethod]
+        public void TestIsDefined()
+        {
+            var g = new Geocache();
+
+            Assert.IsFalse(g.IsDefined());
+
+            g.ShortDescription.Text = "sample";
+
+            Assert.IsTrue(g.IsDefined());
+        }
+    }
+}
