636bf512f52b48a49dd14b887ca74928d22aae83
knagis_cp <SND\knagis_cp>
2012-06-07 09:45:10 +0000

    Added missing license header.

--- Development/GeoTransformer.Extensibility/Transformers/RefreshFromLiveApi/RefreshImagesFromLiveApi.cs
+++ Development/GeoTransformer.Extensibility/Transformers/RefreshFromLiveApi/RefreshImagesFromLiveApi.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿/*
+ * This file is part of GeoTransformer project (http://geotransformer.codeplex.com/).
+ * It is licensed under Microsoft Reciprocal License (Ms-RL).
+ */
+ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace GeoTransformer.Transformers.RefreshFromLiveApi
 {
