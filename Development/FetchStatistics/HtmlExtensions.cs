b01ca9bfd43ce83c71cfb5ca91ae011fa6407f77
knagis_cp <SND\knagis_cp>
2016-03-30 16:19:21 +0000

    Updated statistics download

---
+++ Development/FetchStatistics/HtmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FetchStatistics
+{
+    internal static class HtmlExtensions
+    {
+        public static IEnumerable<HtmlElement> GetElementsByClassName(this HtmlDocument doc, string tagName, string className)
+        {
+            return doc.GetElementsByTagName(tagName)
+                .OfType<HtmlElement>()
+                .Where(o => string.Equals(o.GetAttribute("className"), className, StringComparison.Ordinal));
+        }
+    }
+
+}
